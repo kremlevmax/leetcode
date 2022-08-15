162a986a117587b72c3eee615544b5011d1d5c20
kremlevmax <kremlevmax1989@gmail.com>
2022-08-15 00:16:06 -0700

    Name Fix

--- 1. Pattern: Sliding Window/Smallest_Subarray_with_a_given_sum.js
+++ 1. Pattern: Sliding Window/Smallest_Subarray_with_a_given_sum.js
@@ -1,5 +1,5 @@
 /*
-Maximum Sum subarray of Size
+Smallest Subarray with a given sum
 Given an array of positive integers nums and a positive integer target,
 return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr]
 of which the sum is greater than or equal to target. If there is no such subarray, return 0 instead.
